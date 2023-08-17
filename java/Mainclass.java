450a851af032bfb05f8c1b743fec428cdc0629df
shwetakambure <shwetabure@gmail.com>
2023-08-17 16:40:51 +0530

    first file

---
+++ java/Mainclass.java
@@ -0,0 +1,29 @@
+class Mainclass{
+static void overloadedMethod(A a)
+{
+System.out.println("ONE");
+}
+static void overloadedMethod(B b)
+{
+System.out.println("TWO");
+}
+static void overloadedMethod(Object obj)
+{
+  System.out.println("THREE");
+}
+public static void main(String[] args)
+{
+C c=new C();
+overloadedMethod(c);
+}
+}
+class A 
+{
+
+}
+class B extends A 
+{
+}
+class C extends B
+{
+}
